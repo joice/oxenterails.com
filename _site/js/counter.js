11851393d7c46d102e24c17306f5afe595827471
Joice Taciana <joicetaciana@gmail.com>
2010-05-10 17:04:04 -0300

    Removing files about _site

--- _site/js/counter.js
+++
@@ -1,2 +0,0 @@
-var difference = (new Date("10/17/2010") - new Date()) / 1000;
-document.getElementById("countdown").innerHTML = Math.floor(( difference < 0 ? 0 : difference ) / (60 * 60 * 24)) + 1;
