3357b90c6f8035bbb99b0ad3b1e043de47c46b50
Joice Taciana <joicetaciana@gmail.com>
2010-05-10 13:22:41 -0300

    create the layout and css style's site

---
+++ _site/js/gridder.js
@@ -0,0 +1,18 @@
+var gOverride = {
+  urlBase: 'http://gridder.andreehansson.se/releases/latest/',
+  gColor: 'red',
+  gColumns: 12,
+  gOpacity: 0.25,
+  gWidth: 10,
+  pColor: '#C0C0C0',
+  pHeight: 15,
+  pOffset: 0,
+  pOpacity: 0.55,
+  center: true,
+  gEnabled: true,
+  pEnabled: true,
+  setupEnabled: true,
+  fixFlash: true,
+  size: 960,
+  invert: true
+};
